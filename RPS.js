d5a1b708530333de4d44f578e79d992b352b7bb6
Thomas Tempel <thomastempel@Thomass-Air.hsd1.il.comcast.net>
2022-12-01 13:59:11 -0600

    Saving changes

--- RPS.js
+++ RPS.js
@@ -2,53 +2,68 @@ let playerscore=0
 let computerscore=0
 
 const computerchoice =(compchoice)=> {
-    const choices=["Rock", "Paper", "Scissors"]
+    const choices=["rock", "paper", "scissors"]
     const random=Math.floor(Math.random()*3)
     compchoice=choices[random]
-    console.log(compchoice)
+    return compchoice
 }
 
 const play = (player, computerplay) => {
-        if (player==="rock" && computerplay==="Rock") {
+        if (player==="rock" && computerplay==="rock") {
             return ("You tie! You both chose Rock")
         }
-        else if (player==="rock" && computerplay==="Paper") {
+        //No points for a tie.
+        else if (player==="rock" && computerplay==="paper") {
+            computerscore++
             return ("You lose! Paper covers rock.")
         }
-        else if (player==="pock" && computerplay === "Scissors"){
+        else if (player==="rock" && computerplay === "scissors"){
+            playerscore++
             return ("You win! Rock crushes scissors")
         }
-        else if (player==="paper" && computerplay==="Rock"){
+        else if (player==="paper" && computerplay==="rock"){
+            playerscore++
             return ("You win! Paper covers Rock")
-        }
-        else if (player==="paper" && computerplay==="Paper"){
+        }      
+        else if (player==="paper" && computerplay==="paper"){
             return ("You tie! Paper is Paper")
         }
-        else if (player==="paper" && computerplay==="Scissors"){
+        //No points.
+        else if (player==="paper" && computerplay==="scissors"){
+            computerscore++
             return ("You lose! Scissors cuts paper")
         }
-        else if (player==="scissors" && computerplay==="Paper"){
+        else if (player==="scissors" && computerplay==="paper"){
+            playerscore++
             return ("You win! Scissors cuts paper")
         }
-        else if (player==="scissors" && computerplay=="Rock") {
+        else if (player==="scissors" && computerplay=="rock") {
+            computerscore++
             return ("You lose! Rock crushes Scissors")
         }
-        else if (player==="scisors" && computerplay==="Scissors") {
+        else if (player==="scissors" && computerplay==="scissors") {
             return ("You tie! Scissors are Scissors")
         }
+        //No points. 
     }
 
-    const player = "rock"
-    const  computerplay=computerchoice()
+    const player = "rock";
+    const computerplay = computerchoice();
+    
+    game = () => {
+    for (let i = 0; i < 5; i++) {
+        console.log(play(player, computerplay));
+     }
+     if (playerscore>computerscore){
+     return ("You win!")
+     }
+     else if(computerscore>playerscore){
+        return ("You suck ass! Try again.")
+     }
+     }
 
-    const game = () => {
-        for (let i = 0; i < 5; i++) {
-
-         }
-    }
-
-    game()
-
+game()
+    
 
 
 
