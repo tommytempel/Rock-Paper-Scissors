a35f1e71f2182fe57f2cfaeee3b677550198d4e8
Thomas Tempel <thomastempel@Thomass-Air.hsd1.il.comcast.net>
2022-12-01 00:24:51 -0600

    save

--- RPS.js
+++ RPS.js
@@ -1,3 +1,6 @@
+let playerscore=0
+let computerscore=0
+
 const computerchoice =(compchoice)=> {
     const choices=["Rock", "Paper", "Scissors"]
     const random=Math.floor(Math.random()*3)
@@ -5,26 +8,48 @@ const computerchoice =(compchoice)=> {
     console.log(compchoice)
 }
 
-const letsplay={
-    if (player==="Rock" && computerchoice==="Rock") {
-        return ("You tie! You both chose Rock")
+const play = (player, computerplay) => {
+        if (player==="rock" && computerplay==="Rock") {
+            return ("You tie! You both chose Rock")
+        }
+        else if (player==="rock" && computerplay==="Paper") {
+            return ("You lose! Paper covers rock.")
+        }
+        else if (player==="pock" && computerplay === "Scissors"){
+            return ("You win! Rock crushes scissors")
+        }
+        else if (player==="paper" && computerplay==="Rock"){
+            return ("You win! Paper covers Rock")
+        }
+        else if (player==="paper" && computerplay==="Paper"){
+            return ("You tie! Paper is Paper")
+        }
+        else if (player==="paper" && computerplay==="Scissors"){
+            return ("You lose! Scissors cuts paper")
+        }
+        else if (player==="scissors" && computerplay==="Paper"){
+            return ("You win! Scissors cuts paper")
+        }
+        else if (player==="scissors" && computerplay=="Rock") {
+            return ("You lose! Rock crushes Scissors")
+        }
+        else if (player==="scisors" && computerplay==="Scissors") {
+            return ("You tie! Scissors are Scissors")
+        }
     }
-    else if (player==="Rock" && computerchoice==="Paper") {
-        return ("You lose! Paper covers rock.")
+
+    const player = "rock"
+    const  computerplay=computerchoice()
+
+    const game = () => {
+        for (let i = 0; i < 5; i++) {
+
+         }
     }
-    else if (player==="Rock" && computerchoice === "Scissors"){
-        return ("You win! Rock crushes scissors")
-    }
-    else if (player==="Paper" && computerchoice==="Rock"){
-        return ("You win! Paper covers Rock")
-    }
-    else if (player==="Paper" && computerchoice==="Paper"){
-        return ("You tie! Paper is Paper")
-    }
-    else if (player==="paper" && computerchoice==="Scissors"){
-        return ("You lose! Scissors cuts paper")
-    }
-    else if (player==="Scissors" && computerchoice==="Paper"){
-        return ("You win! Scissors cuts paper")
-    }
-}
+
+    game()
+
+
+
+
+
